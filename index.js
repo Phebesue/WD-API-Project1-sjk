3e9ee7c36fb0c678bf4d09d99131f401ed6343f9
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-08-12 17:50:43 -0400

    got images to come in from API & display in a grid

---
+++ index.js
@@ -0,0 +1,32 @@
+//Documentation: https://picsum.photos/;
+
+// const baseURL= "https://picsum.photos/v2/list";
+// const baseURL= "https://picsum.photos/200/300?random=";
+const baseURL = "https://picsum.photos/v2/list?page=3&limit=40";
+const masonry = document.querySelector(".masonry");
+
+fetch(baseURL)
+  .then((result) => {
+    return result.json();
+    console.log(result);
+  })
+  .then((data) => {
+    displayImages(data);
+  })
+  .catch((err) => console.log(err));
+
+function displayImages(passedData) {
+  passedData.forEach((element) => {
+    // console.log(element);
+    let box = document.createElement("div");
+    let img = document.createElement("img");
+
+    box.setAttribute("class", "masonry-item");
+    console.log(element.download_url);
+    img.src = element.download_url;
+    masonry.appendChild(box);
+    box.appendChild(img);
+    img.alt = element.url;
+    img.setAttribute("class", "masonry-content");
+  });
+}
